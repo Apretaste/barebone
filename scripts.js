924dc04885c31f7fdf7554502438f740fa36b714
Salvi Pascual <salvi.pascual@gmail.com>
2020-10-11 12:25:40 -0400

    upload tags in submit

--- scripts.js
+++ scripts.js
@@ -4,3 +4,21 @@ $(document).ready(function() {
 	$('select').formSelect();
 	$('.sidenav').sidenav();
 });
+
+// count remaining characteres
+//
+function remainder (size=200) {
+	// get message and remainder amount
+	var message = $('#message').val().trim();
+	var remainder = (message.length <= size) ? (size - message.length) : 0;
+
+	// restrict message size
+	if (remainder <= 0) {
+		message = message.substring(0, size);
+		$('#message').val(message);
+	}
+
+	// update remainder amount
+	$('#remainder').html(message.length);
+}
+
